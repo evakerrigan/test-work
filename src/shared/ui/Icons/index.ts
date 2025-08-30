15809132fc9fdc9f775a61dec0cd4ae7352f1576
Eva Kerrigan <evakerrigan@gmail.com>
2025-08-30 05:45:40 +0300

    chore: refactor imports

--- src/shared/ui/Icons/index.ts
+++ src/shared/ui/Icons/index.ts
@@ -1,5 +1,5 @@
 export { LogoTreeIcon } from './LogoTreeIcon';
-export { CompanyIcon } from './CompanyIcon';
+export { OrganizationIcon } from './OrganizationIcon';
 export { ContractorIcon } from './ContractorIcon';
 export { UserIcon } from './UserIcon';
 export { SearchIcon } from './SearchIcon';
